5fa5cdeb423f4ad8ced23428bb3edaa119c6554f
hnim71 <minh.dt2416808@sis.hust.edu.vn>
2026-05-24 19:51:47 +0700

    Update app.py
    Removed vulnerable configuration options and added security headers.

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
 from config import vuln_app
 import os
+from flask import jsonify
 
-'''
- Decide if you want to server a vulnerable version or not!
- DO NOTE: some functionalities will still be vulnerable even if the value is set to 0
-          as it is a matter of bad practice. Such an example is the debug endpoint.
-'''
-vuln = int(os.getenv('vulnerable', 1))
-# vuln=1
-# token alive for how many seconds?
-alive = int(os.getenv('tokentimetolive', 60))
+app = vuln_app.app
 
+@app.after_request
+def add_security_headers(response):
+    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
+    response.headers['Cross-Origin-Resource-Policy'] = 'same-origin' 
+    return response
 
-# start the app with port 5000 and debug on!
 if __name__ == '__main__':
-    vuln_app.run(host='0.0.0.0', port=5000, debug=True)
+    vuln_app.run(host='0.0.0.0', port=5000, debug=False)
